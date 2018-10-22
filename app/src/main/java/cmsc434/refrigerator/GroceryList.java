7c9627f7065f9fbabc4a5261c3fdbec1e331e4de
Jake Chavis <jakechavis6@gmail.com>
2018-10-22 13:37:45 -0400

    first real commit... to show how we should be adding pages and xml files.

---
+++ app/src/main/java/cmsc434/refrigerator/GroceryList.java
@@ -0,0 +1,12 @@
+package cmsc434.refrigerator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GroceryList extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.grocery_list);
+    }
+}
