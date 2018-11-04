bf07a60fa577f9bb6525f0e9e90cd3d877d842eb
John Shi <johnshi5@gmail.com>
2018-11-04 12:14:28 -0500

    Johns meal planning pages

--- app/src/main/java/cmsc434/refridgerator/RecipeActivity.java
+++ app/src/main/java/cmsc434/refridgerator/RecipeActivity.java
@@ -1,13 +1,20 @@
 package cmsc434.refridgerator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import cmsc434.refridgerator.MealPlanning.RecipeSelectionActivity;
 import cmsc434.refridgerator.adapters.RecipeRecyclerViewAdapter;
 import cmsc434.refridgerator.model.RecipeEntry;
 
@@ -19,14 +26,24 @@ public class RecipeActivity extends baseNav implements RecipeRecyclerViewAdapter
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recipe_activity);
+
+
+        FrameLayout contentFrame = findViewById(R.id.content_frame);
+        getLayoutInflater().inflate(R.layout.recipe_activity, contentFrame, true);
+        Menu menu = navView.getMenu();
+        MenuItem mi = menu.findItem(R.id.nav_meals);
+        mi.setChecked(true);
+
+        TextView title = findViewById(R.id.title);
+        title.setText("Meal Planning - Recipes");
 
         List<RecipeEntry> entries = new ArrayList<>();
-        entries.add(new RecipeEntry(R.drawable.pasta, "pasta1"));
-        entries.add(new RecipeEntry(R.drawable.pasta, "pasta2"));
-        entries.add(new RecipeEntry(R.drawable.pasta, "pasta3"));
-        entries.add(new RecipeEntry(R.drawable.pasta, "pasta4"));
-        entries.add(new RecipeEntry(R.drawable.pasta, "pasta5"));
+        entries.add(new RecipeEntry(R.drawable.salmon, "salmon cakes"));
+        entries.add(new RecipeEntry(R.drawable.pasta, "pasta"));
+        entries.add(new RecipeEntry(R.drawable.pumpkin, "pumpkin and sausage soup"));
+        entries.add(new RecipeEntry(R.drawable.mushroompork, "mushroom pork chops"));
+        entries.add(new RecipeEntry(R.drawable.padthai, "sukhothai pad thai"));
+        entries.add(new RecipeEntry(R.drawable.limechicken, "spicy garlic lime chicken"));
 
         int cols = 2;
 
@@ -39,7 +56,13 @@ public class RecipeActivity extends baseNav implements RecipeRecyclerViewAdapter
     }
 
     public void onItemClick(View view, int position) {
-        System.out.println(position);
+
+        Intent intent = new Intent(RecipeActivity.this, RecipeSelectionActivity.class);
+        intent.putExtra("recipe", ""+position);
+
+        startActivity(intent);
+
+
     }
 
 
