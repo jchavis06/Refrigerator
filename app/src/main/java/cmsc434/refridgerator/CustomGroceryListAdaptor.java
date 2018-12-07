4160532961d288c911d2f470cc9f38bc0e8f2531
Jake Chavis <jakechavis@wireless-10-105-180-110.umd.edu>
2018-12-06 22:02:50 -0500

    added delete functionality

--- app/src/main/java/cmsc434/refridgerator/CustomGroceryListAdaptor.java
+++ app/src/main/java/cmsc434/refridgerator/CustomGroceryListAdaptor.java
@@ -1,12 +1,19 @@
 package cmsc434.refridgerator;
 
+import android.app.AlertDialog;
 import android.content.ClipData;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -37,7 +44,7 @@ public class CustomGroceryListAdaptor extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         // inflate the layout for each list row
         if (convertView == null) {
             convertView = LayoutInflater.from(context).
@@ -45,7 +52,7 @@ public class CustomGroceryListAdaptor extends BaseAdapter {
         }
 
         // get current item to be displayed
-        GroceryListItem currentItem = (GroceryListItem) getItem(position);
+        final GroceryListItem currentItem = (GroceryListItem) getItem(position);
 
         // get the TextView for item name and item description
         TextView textViewItemName = (TextView)
@@ -56,6 +63,7 @@ public class CustomGroceryListAdaptor extends BaseAdapter {
                 convertView.findViewById(R.id.itemQuantityTypeView);
         TextView textViewItemBrand = (TextView)
                 convertView.findViewById(R.id.itemBrandView);
+        ImageButton deleteButton = (ImageButton) convertView.findViewById(R.id.deleteButton);
 
         //sets the text for item name and item description from the current item object
         textViewItemName.setText(currentItem.getItemName());
@@ -63,6 +71,44 @@ public class CustomGroceryListAdaptor extends BaseAdapter {
 
         textViewItemQuantityType.setText(currentItem.getQuantityType());
         textViewItemBrand.setText("" + currentItem.getBrand());
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    builder = new AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_Alert);
+                } else {
+                    builder = new AlertDialog.Builder(context);
+                }
+                builder.setTitle("Add Item")
+                        .setMessage("Are you sure you want to delete '" + currentItem.getItemName() + "'?")
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // continue with delete
+                                GroceryListWriter glw = new GroceryListWriter(context);
+                                glw.removeFromGroceryList(currentItem.getItemName());
+                                items.remove(position);
+                                notifyDataSetChanged();
+
+                                Toast toast = Toast.makeText(context,
+                                        "Successfully deleted '" + currentItem.getItemName() + "'",
+                                        Toast.LENGTH_SHORT);
+                                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                                toast.show();
+                            }
+                        }).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            //don't delete anything
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+                //Intent newIntent = new Intent(add_grocery_item.class, CustomGroceryListAdaptor.this);
+                //context.startActivity(newIntent);
+            }
+        });
         // returns the view for the current row
         return convertView;
     }
